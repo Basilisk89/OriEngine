61af093e3600b013af248514b93f8dcc0b04b62d
Ryan AJ Holubeshen <Ryan.aj.holubeshen@bell.net>
2016-09-16 14:01:12 -0400

    Debugger
    Added Debugger

--- OriEngine/OriEngine/Main.cpp
+++ OriEngine/OriEngine/Main.cpp
@@ -1,7 +1,10 @@
 #include <glew-2.0.0\include\GL\glew.h>
 #include <Assimp\include\assimp\Importer.hpp>
-
 #include<glm\glm\vec3.hpp>
+#include "DebugLogger.h"
+using namespace OriEngine;
 int main(int args, char* argv[]) {
+	DebugLogger::getInstance().log(DebugLogger::MsgType::INFO, "Main", "Main", __FILE__, __LINE__, "Main Started");
+	DebugLogger::getInstance().log(DebugLogger::MsgType::ERROR, "Main", "Main", __FILE__, __LINE__, "Test of Error");
 	return 0;
 }
